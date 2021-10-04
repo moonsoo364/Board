b3c24bd61088423fd33003957ac156f0d54cb294
DESKTOP-V47AKJU\aptx1 <aptx15hibr@naver.com>
2021-10-04 23:02:17 +0900

    1004 백업

---
+++ TestPjt/WebContent/Homepage_dev/javascript/update.js
@@ -0,0 +1,24 @@
+function writeCheck()
+{
+	var form =document.updateform;
+	
+	if(!form.title.value)
+	{
+		alert("제목을 적어주세요");
+		form.title.focus();
+		return;
+	}
+	else if(!form.content.value)
+	{
+		alert("내용을 적어주세요"); 
+		form.content.focus();
+		return;
+	}
+	else
+	{
+		document.updateform.submit();
+	}
+	
+	
+	
+}
